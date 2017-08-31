ba0fa0e20862c0c6526845443830072f1edb62bc
Yuvaraj <shivarajnaidu@gmail.com>
2017-08-31 07:35:07 +0530

    Added binding of param values to routeLink directive

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -7,8 +7,11 @@ import { UserService } from '../services/user.service';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
+
+
+
 export class UserListComponent implements OnInit {
-  users: User[];
+  users: User[] = [];
   constructor(private userService: UserService) {
   	
   }
