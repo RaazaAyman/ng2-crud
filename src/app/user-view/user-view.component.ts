96012cffd36c6fc2db43c5e0fcb3cc5e6a782f11
Yuvaraj <shivarajnaidu@gmail.com>
2017-09-01 08:19:31 +0530

    User View Component Working Now

--- src/app/user-view/user-view.component.ts
+++ src/app/user-view/user-view.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location }                 from '@angular/common';
+import { UserService }     from '../services/user.service';
+import { User }     from '../services/user';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-user-view',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-view.component.css']
 })
 export class UserViewComponent implements OnInit {
-
-  constructor() { }
+  user: User;
+  userKeys: string[];
+  constructor(
+  private userService: UserService,
+  private route: ActivatedRoute,
+  private location: Location
+  	) { }
 
   ngOnInit() {
+  	 this.route.paramMap
+    .switchMap((params: ParamMap) => this.userService.getUserById(+params.get('id')))
+    .subscribe(user => {
+    	this.user = user;
+    	this.userKeys = Object.keys(user);
+    });
   }
 
 }
